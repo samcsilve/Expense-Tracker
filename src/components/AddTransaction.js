a6bcf1be454bf5ca85e12ed571d48d5f1c2a49de
samcsilve <65583508+samcsilve@users.noreply.github.com>
2020-06-20 19:02:34 -0700

    expense tracker commit

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,49 @@
+import React, { useState, useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+
+
+export const AddTransaction = () => {
+    const [text, setText] = useState('')
+    const [amount, setAmount] = useState(0)
+
+    const { addTransaction } = useContext(GlobalContext)
+
+    const onSubmit = event => {
+        event.preventDefault()
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 100000000),
+            text,
+            amount: +amount,
+        }
+
+        addTransaction(newTransaction)
+    }
+
+    return (
+        <div>
+            <h3>Add new transaction</h3>
+            <form onSubmit={onSubmit}>
+                <div className="form-control">
+                    <label htmlFor="text">Text</label>
+                    <input
+                        type="text"
+                        value={text}
+                        onChange={(event) => setText(event.target.value)}
+                        placeholder="Enter text..." />
+                </div>
+                <div className="form-control">
+                    <label htmlFor="amount">Amount
+                    <br />
+            (negative - expense, positive - income) </label>
+                    <input
+                        value={amount}
+                        onChange={(event) => setAmount(event.target.value)}
+                        type="number"
+                        placeholder="Enter amount..." />
+                </div>
+                <button className="btn">Add transaction</button>
+            </form>
+        </div>
+    )
+}
